c4ca0269c464373fa54ae852c591b7d85ae16d22
ndorazio137 <ndorazio@csumb.edu>
2021-05-18 01:22:42 -0700

    Initial Commit

---
+++ src/main/java/cst438/Cst438w3producerApplication.java
@@ -0,0 +1,13 @@
+package cst438;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438w3producerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst438w3producerApplication.class, args);
+	}
+
+}
